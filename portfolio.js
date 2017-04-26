b423fae5acb7fc03a8b37c00954c279908f7ce7d
Aleks <aleks.stefanovic101@gmail.com>
2017-04-26 17:37:46 -0400

    mostly good progress on site

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
     $.get("imports/nav.html", function(data){
         $("#nav").replaceWith(data);
+        console.log(location.pathname);
+        $("a[href='" + location.pathname + "']").addClass("current");
     });
 });
