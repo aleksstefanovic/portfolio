3089fdd8cecf890299b84290a8f6adb61bcf7bd9
Aleks <aleks.stefanovic101@gmail.com>
2017-04-30 01:02:17 -0400

    with contact form working

--- client/portfolio.js
+++ client/portfolio.js
@@ -10,3 +10,24 @@ $(document).ready(function() {
         $("a[href='" + location.pathname + "']").addClass("current");
     });
 });
+
+function validateContact () {
+    var name = document.forms["contactform"]["name"].value;
+    var email = document.forms["contactform"]["email"].value;
+    var body = document.forms["contactform"]["body"].value;
+    //console.log(name,email,body);
+
+    if (!name) {
+        alert ("You must provide a name");
+        return false;
+    }
+    if (!email) {
+        alert ("You must provide an email address");
+        return false;
+    }
+    if (!body) {
+        alert ("You must provide an body");
+        return false;
+    }
+    return true;
+}
